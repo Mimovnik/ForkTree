13f7df4e2de33cecd0a4fc2da20283707b18caac
mimovnik <mimovnik@protonmail.com>
2024-03-22 23:08:04 +0100

    Issue fork() fail

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <stdio.h>
 
 void grow_leaf();
 void grow_branch();
@@ -27,6 +28,10 @@ int main() {
 // creates a new proccess that waits for its parent to finish
 void grow_leaf() {
   int pid = fork();
+  if(pid == -1) {
+    perror("fork() failed\n");
+    exit(1);
+  }
   if(pid == 0) {
     waitpid(getppid(), NULL, 0);
     exit(0);
@@ -36,6 +41,10 @@ void grow_leaf() {
 // creates a new proccess and waits for it to finish
 void grow_branch() {
   int pid = fork();
+  if(pid == -1) {
+    perror("fork() failed\n");
+    exit(1);
+  }
   if(pid != 0) {
     waitpid(pid, NULL, 0);
     exit(0);
@@ -45,6 +54,10 @@ void grow_branch() {
 // creates a new proccess and waits for it to finish also creates a leaf before waiting
 void grow_branch_and_leaf(){
   int pid = fork();
+  if(pid == -1) {
+    perror("fork() failed\n");
+    exit(1);
+  }
   if(pid != 0) {
     grow_leaf();
     waitpid(pid, NULL, 0);
